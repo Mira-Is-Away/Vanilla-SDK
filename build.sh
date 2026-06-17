c1eb00420524cf213ba776adb4d9b3e1d0105976
henry <152448987+henryisaway@users.noreply.github.com>
2026-06-17 13:32:39 -0300

    extra: helper scripts cleanup

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# Exit on error
 set -e
 
-# Create build directory if it doesn't exist
 mkdir -p bin
 
-# Configure and build
 cd bin
 cmake ..
 make -j"$(nproc)"
-
-# Run the program (optional, might fail if dependencies are missing at runtime)
-# ./vanilla
--- memcheck.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e 
-
-valgrind --leak-check=full --show-leak-kinds=all ./bin/vanilla
