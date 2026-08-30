3dc8904529ae3decae5a341d65befcfdeb0b4930
henry <152448987+henryisaway@users.noreply.github.com>
2026-08-29 21:03:52 -0300

    feat(vulkan): command pool creation

---
+++ include/vulkan/vkcommandpool.h
@@ -0,0 +1,16 @@
+#ifndef VNL_VULKAN_VKCOMMANDPOOL_H_
+#define VNL_VULKAN_VKCOMMANDPOOL_H_
+
+#include <core/vnl_status.h>
+#include <vulkan/vulkan.h>
+
+typedef struct {
+    VkDevice device;
+    VkPhysicalDevice physical_device;
+    VkSurfaceKHR surface;
+} VkCommandPoolDesc;
+
+VnlStatus vk_command_pool_create(const VkCommandPoolDesc *desc,
+                                 VkCommandPool *out_command_pool);
+
+#endif
